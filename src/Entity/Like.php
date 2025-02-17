b72752f690f65b96158e513b05af8d6ee7d8d1f0
Jensone <hello@jensone.com>
2025-02-17 15:45:41 +0100

    Like entity

---
+++ src/Entity/Like.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LikeRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\UX\Turbo\Attribute\Broadcast;
+
+#[ORM\Entity(repositoryClass: LikeRepository::class)]
+#[ORM\Table(name: '`like`')]
+#[Broadcast]
+class Like
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'likes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Todo $todo = null;
+
+    #[ORM\ManyToOne(inversedBy: 'likes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $creator = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTodo(): ?Todo
+    {
+        return $this->todo;
+    }
+
+    public function setTodo(?Todo $todo): static
+    {
+        $this->todo = $todo;
+
+        return $this;
+    }
+
+    public function getCreator(): ?User
+    {
+        return $this->creator;
+    }
+
+    public function setCreator(?User $creator): static
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+}
