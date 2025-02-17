597a958c45d417d720df983551ad3333410d3b3e
Jensone <hello@jensone.com>
2025-02-17 16:08:44 +0100

    Task entity

---
+++ src/Entity/Task.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\UX\Turbo\Attribute\Broadcast;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[Broadcast]
+#[ORM\HasLifecycleCallbacks]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?bool $is_done = null;
+
+    #[ORM\Column]
+    private ?bool $is_public = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $time_due = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tasks')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Todo $todo = null;
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): void
+    {
+        $this->created_at = new \DateTimeImmutable();
+        $this->updated_at = new \DateTimeImmutable();
+    }
+
+    #[ORM\PreUpdate]
+    public function setUpdatedAtValue(): void
+    {
+        $this->updated_at = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function isDone(): ?bool
+    {
+        return $this->is_done;
+    }
+
+    public function setIsDone(bool $is_done): static
+    {
+        $this->is_done = $is_done;
+
+        return $this;
+    }
+
+    public function isPublic(): ?bool
+    {
+        return $this->is_public;
+    }
+
+    public function setIsPublic(bool $is_public): static
+    {
+        $this->is_public = $is_public;
+
+        return $this;
+    }
+
+    public function getTimeDue(): ?\DateTimeInterface
+    {
+        return $this->time_due;
+    }
+
+    public function setTimeDue(?\DateTimeInterface $time_due): static
+    {
+        $this->time_due = $time_due;
+
+        return $this;
+    }
+
+    public function getTodo(): ?Todo
+    {
+        return $this->todo;
+    }
+
+    public function setTodo(?Todo $todo): static
+    {
+        $this->todo = $todo;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return substr($this->content, 0, 20) . "...";
+    }
+}
