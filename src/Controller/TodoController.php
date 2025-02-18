5f0d411dd6fe0fa53d86f9c3a0f44725ee61b982
Jensone <hello@jensone.com>
2025-02-18 10:52:11 +0100

    Mise en place du contrôleur TodoController avec 3 routes

---
+++ src/Controller/TodoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class TodoController extends AbstractController
+{
+    public function __construct(
+        private TodoRepository $tr,
+        private EntityManagerInterface $em,
+    ){}
+
+    #[Route('/todos', name: 'todos_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('todo/index.html.twig', [
+            'todos' => $this->tr->findByCreator($this->getUser()),
+        ]);
+    }
+    
+    #[Route('/todos/{ref}', name: 'todos_show', methods: ['GET'])]
+    public function show(string $ref): Response
+    {
+        return $this->render('todo/show.html.twig', [
+            'todo' => $this->tr->findByRef($ref),
+        ]);
+    }
+
+    #[Route('/todos/delete/{ref}', name: 'todos_delete', methods: ['POST'])]
+    public function delete(string $ref): Response
+    {
+        $todo = $this->tr->findByRef($ref);
+
+        if($todo->getCreator() === $this->getUser()){
+            $this->em->remove($todo);
+            $this->em->flush();
+
+            $this->addFlash('success', 'La tâche a bien été supprimée');
+        } else {
+            $this->addFlash('danger', 'Cette tâche ne vous appartient pas');
+        }
+
+        return $this->redirectToRoute('todos_index');
+    }
+}
