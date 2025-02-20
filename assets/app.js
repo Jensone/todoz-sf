f5fe9700c63dbf6e7e77cae964a6714e5dc497c8
Jensone <hello@jensone.com>
2025-02-20 14:09:36 +0100

    Amélioration de la gestion du menu : utilisation de l'événement turbo:load et vérification de l'existence des éléments avant d'ajouter des écouteurs d'événements.

--- assets/app.js
+++ assets/app.js
@@ -10,20 +10,20 @@ import './styles/app.css';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
 // Menu toggle
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('turbo:load', function() {
     const menuToggle = document.getElementById('menuToggle');
     const menuClose = document.getElementById('menuClose');
     const menuOverlay = document.querySelector('.menu-overlay');
-    
-    menuToggle.addEventListener('click', function() {
-        menuOverlay.classList.remove('hidden');
-        document.body.classList.add('menu-open');
-    });
 
-    menuClose.addEventListener('click', function() {
-        menuOverlay.classList.add('hidden');
-        document.body.classList.remove('menu-open');
-    });
+    if (menuToggle && menuClose && menuOverlay) {
+        menuToggle.addEventListener('click', function() {
+            menuOverlay.classList.remove('hidden');
+            document.body.classList.add('menu-open');
+        });
+
+        menuClose.addEventListener('click', function() {
+            menuOverlay.classList.add('hidden');
+            document.body.classList.remove('menu-open');
+        });
+    }
 });
-
-
